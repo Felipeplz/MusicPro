c722ad6d4f778013b96c8a6f8778ac6893803a45
PiaFormas <mpiaformas@gmail.com>
2021-06-01 00:07:56 -0400

    primer commit pitty

--- musicpro/views.py
+++ musicpro/views.py
@@ -5,10 +5,9 @@ import pyodbc
 # Create your views here.
 def productosTodos(request):
     conn = pyodbc.connect('Driver={sql server};'
-                        'Server=FELIPE-LEGION\FELIPE;'
-                        'Database=MusicPro;'
-                        'UID=django-user;'
-                        'PWD=%.ZSix:)R:NN5RT')
+                         'Server=LAPTOP-R0CJDD69\PIA;'
+                         'Database=MusicPro;'
+                        'Trusted_Connection=yes')
     cursor = conn.cursor()
     result = cursor.execute("SELECT * FROM PRODUCTO ORDER BY id_producto ASC").fetchall()
-    return render(request, 'Catalogo_Producto.html', {'SQLProductos':result})
+    return render(request, 'Detalle_Producto.html', {'SQLProductos':result})
