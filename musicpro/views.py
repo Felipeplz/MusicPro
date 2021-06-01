4ac36c745bfb35fdcb1ba7d8e88d860128aefc29
luis aravena <lu.aravenab@alumnos.duoc.cl>
2021-06-01 00:01:55 -0400

    reportes a casi listos

--- musicpro/views.py
+++ musicpro/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-from musicpro.models import SQLProductos
+from musicpro.models import SQLProductos, SQLVentas
 import pyodbc
 
 # Create your views here.
 def productosTodos(request):
     conn = pyodbc.connect('Driver={sql server};'
-                        'Server=FELIPE-LEGION\FELIPE;'
+                        'Server=DESKTOP-T62UH2C;'
                         'Database=MusicPro;'
-                        'UID=django-user;'
-                        'PWD=%.ZSix:)R:NN5RT')
+                        'Trusted_Connection=yes')
     cursor = conn.cursor()
     result = cursor.execute("SELECT * FROM PRODUCTO ORDER BY id_producto ASC").fetchall()
     return render(request, 'Catalogo_Producto.html', {'SQLProductos':result})
+
+def ventastodos(request):
+    conn = pyodbc.connect('Driver={sql server};'
+                        'Server=DESKTOP-T62UH2C;'
+                        'Database=MusicPro;'
+                        'Trusted_Connection=yes')
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT * FROM VENTA ORDER BY id_venta ASC").fetchall()
+    return render(request, 'ReporteVentas.html', {'SQLVentas':result})
+
+def ventastodos(request):
+    conn = pyodbc.connect('Driver={sql server};'
+                        'Server=DESKTOP-T62UH2C;'
+                        'Database=MusicPro;'
+                        'Trusted_Connection=yes')
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT * FROM VENTA ORDER BY id_venta ASC").fetchall()
+    return render(request, 'ReporteVentas.html', {'SQLVentas':result})
