d1d2df3201a3226e0a9c9acc010aa09bac02df4e
Felipeplz <f.zavallav@gmail.com>
2021-06-01 02:33:53 -0400

    Catálogo Funcional

--- musicpro/urls.py
+++ musicpro/urls.py
@@ -20,8 +20,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.productosTodos),
-    path('admin/', admin.site.urls)
+    path('', views.viewCatalogo),
+    path('catalogo', views.viewCatalogo),
+    path('catalogo/<str:tab>', views.viewCatalogo),
+    #path('admin/', admin.site.urls)
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- musicpro/views.py
+++ musicpro/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from musicpro.models import SQLProductos
 import pyodbc
 
 # Create your views here.
-def productosTodos(request):
+def viewCatalogo(request, **kwargs):
+    if (request.path == "/"):
+        return redirect('/catalogo')
     conn = pyodbc.connect('Driver={sql server};'
                         'Server=FELIPE-LEGION\FELIPE;'
                         'Database=MusicPro;'
                         'UID=django-user;'
                         'PWD=%.ZSix:)R:NN5RT')
     cursor = conn.cursor()
-    result = cursor.execute("SELECT * FROM PRODUCTO ORDER BY id_producto ASC").fetchall()
-    return render(request, 'Catalogo_Producto.html', {'SQLProductos':result})
+    tab = kwargs.get('tab')
+    if (tab == None):
+        tab = "todos"
+        query = ""
+    else:
+        query = f"WHERE categoria = '{tab}'"
+    result = cursor.execute(f"SELECT * FROM PRODUCTO {query} ORDER BY id_producto ASC").fetchall()
+    return render(request, 'Catalogo_Producto.html', {'SQLProductos':result, 'tab': tab})
