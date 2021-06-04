e8fb89d07d4e5c79a7830cd934ae37ed43a5d39d
fabiolatroncoso <ftroncosoayala@gmail.com>
2021-06-04 16:35:15 -0400

    commit inicial

--- musicpro/urls.py
+++ musicpro/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('usuarioslista/', views.viewUsuarios),
     #path('carrito/', views.productosCarrito),
     #path('admin/', admin.site.urls)
+    
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
